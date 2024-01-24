e81b31fd89c66ac7e18d2a6da665e641c531fae0
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-01-24 17:52:12 +0530

    created a responsive navbar and side bar

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const sideBar = document.getElementById("sideBar");
+const sideNavBar = document.getElementById("sideNavBar");
 const toggleBtn = document.getElementById("toggleBtn");
-const version = document.getElementById("version");
+const toggleMobileNavBar = document.getElementById("toggleMobileNavBar");
+const currTool = document.getElementById("currTool");
 
 const toggleSideBar = (e) => {
-   let width = sideBar.style.width ; 
-   if(width == "55px"){
-    sideBar.style.width = "250px";
-    version.style.display = "block";
-}
-else{
-    sideBar.style.width = "55px";
-    version.style.display = "none";
-   }
+    let width = sideBar.style.width;
+    if (width == "55px") {
+        sideBar.style.width = "250px";
+        sideBar.classList.remove("closeText");
+    }
+    else {
+        sideBar.style.width = "55px";
+        sideBar.classList.add("closeText");
+    }
 }
 
-toggleBtn.addEventListener('click',toggleSideBar);
+const toggleMobileSideBar = (e) => {
+    let width = sideNavBar.style.width;
+    if (width == "0px") {
+        sideNavBar.style.width = "250px";
+    }
+    else {
+        sideNavBar.style.width = "0px";
+    }
+}
+
+toggleBtn.addEventListener('click', toggleSideBar);
+toggleMobileNavBar.addEventListener('click', toggleMobileSideBar);
+
+const tools = document.querySelectorAll(".tool");
+
+Array.from(tools).forEach((element)=>{
+    element.addEventListener('click',()=>{
+        currTool.innerText = element.querySelector('h3').innerText;
+    })
+})
